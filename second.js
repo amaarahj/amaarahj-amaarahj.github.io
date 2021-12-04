c60b819cb72dc08d4c24c0da52f9c39c420105c4
amaarah2 <amaarah2@illinois.edu>
2021-12-04 13:50:04 -0500

    added other pages

--- second.js
+++ second.js
@@ -8,7 +8,6 @@ var svg = d3.select("#my_dataviz")
 .append("g")
     .attr("transform",
         "translate(" + margin + "," + margin + ")");
-// get type for each year and filter by make
 function filterJSON(json, key, selected) {
     var result = [];            
         
@@ -17,60 +16,53 @@ function filterJSON(json, key, selected) {
         .key(function(d) {
             return d["Transmission Type"];
         })
+        .key(function(d) {
+            return d.Year;
+        })
         .sortKeys(d3.ascending)
         .rollup(function(v) {return {  
             count: v.length
         };})
         .entries(json)
-        .map(function(v){
-                return{
-                    "TransType": v.key,
-                    "Count": v.value.count
-                }
-            })
+
         json = JSON.stringify(Makes)
         json = JSON.parse(json);
-        result =[{
-            "Make": "All",
-            "data": json
-        }]
+        console.log(json)
+        result = json.filter(function(d){
+            return d.key == key;
+        });
     }else{
         var Makes = d3.nest()
         .key(function(d) {
-            return d.Make;
+            return d["Transmission Type"];
         })
         .key(function(d) {
-            return d["Transmission Type"];
+            return d.Make;
+        })
+                .key(function(d) {
+            return d.Year;
         })
         .sortKeys(d3.ascending)
         .rollup(function(v) {return {  
             count: v.length
         };})
         .entries(json)
-        .map(function(group){
-            return{
-                "Make": group.key,
-                "data": group.values.map(function(v){
-                    // console.log(v);
-                    return{
-                        "TransType": v.key,
-                        "Count": v.value.count
-                    }
-                })
-            };
-        });
+        ;
         var json = JSON.stringify(Makes)
         var json = JSON.parse(json);
-        // console.log(json)
+        console.log(json)
         result = json.filter(function(d){
-            return d.Make == selected;
+            return d.key == key;
+        })
+        console.log(result[0].values)
+        result = result[0].values.filter(function(d){
+            return d.key == selected;
         });
     }
         
     console.log(result);
     return result;
 }
-
 function AddDropDownList(makes) {    
     var ddlMakes = document.getElementById("inds");
     var option = document.createElement("OPTION");
@@ -93,10 +85,16 @@ function AddDropDownList(makes) {
 
 d3.csv("./data.csv", function(data) {   
     var sumstat = d3.nest() // nest function allows to group the calculation per level of a factor
+        .key(function(d) {
+            return d["Transmission Type"];
+        })
         .key(function(d) { return d.Make;})
         .sortKeys(d3.ascending)
         .entries(data);
-    var res = sumstat.map(function(d){ return d.key }) // list of make names
+    sumstat = sumstat.filter(function(d){
+        return d.key == 'MANUAL';
+    })
+    var res = sumstat[0].values.map(function(d){ return d.key }) // list of make names
     console.log(res);
     AddDropDownList(res)
 
@@ -105,13 +103,13 @@ d3.csv("./data.csv", function(data) {
         var sect = document.getElementById("inds");
         var section = sect.options[sect.selectedIndex].value;
         console.log(section)
-        fd = filterJSON(data, 'Make', section);
+        fd = filterJSON(data, 'MANUAL', section);
                     
         d3.selectAll("g>*").remove();
         updateGraph(data, fd);
     });
-    // middle not used?
-    fd = filterJSON(data, 'Make', 'All');
+    // middle used as global
+    fd = filterJSON(data, 'MANUAL', 'All');
     updateGraph(data, fd);
 
 })
@@ -120,21 +118,22 @@ function updateGraph(data, sumstat){
     var yy = d3.nest() // nest function allows to group the calculation per level of a factor
         .key(function(d) { return d.Year;})
         .sortKeys(d3.ascending)
-        .entries(data);
+        .entries(data);    
     var ry = yy.map(function(d){ return d.key }) // list of make names
-    // Add X axis --> it is a date format
-    var x = d3.scaleBand()
-        // .domain(d3.extent(data, function(d) { return d.Year; }))
+    console.log(sumstat)// Add X axis --> it is a date format
+    var x = d3.scaleTime()
+    .domain(d3.extent(sumstat[0].values, function(d) {return d.key; }))
+    // .domain(d3.extent(data, function(d) { return d.Year; }))
         .range([ 0, width ])  
-        .domain(sumstat[0].data.map(function(d) { return d.TransType; }))
-        .padding(0.2);
+        // .domain(sumstat[0].data.map(function(d) { return d.TransType; }))
+        // .padding(0.2);
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-    // Add Y axis .tickValues(ry).tickFormat(d3.format(","))
-    // console.log(sumstat[0].data)
+        .call(d3.axisBottom(x).tickValues(ry).tickFormat(d3.format("")));
+    // Add Y axis 
+    console.log(sumstat[0].values)
     var y = d3.scaleLinear()
-        .domain([0, 1.1*d3.max(sumstat[0].data, function(d) { return d.Count; })])
+        .domain([0, 1.1*d3.max(sumstat[0].values, function(d) { return d.value.count; })])
         .range([ height, 0 ]);
     // if (sumstat.length ==48){
     //     y= d3.scaleLinear()
@@ -144,21 +143,22 @@ function updateGraph(data, sumstat){
     svg.append("g")
     .call(d3.axisLeft(y));
     var u = svg.selectAll("rect")
-        .data(sumstat[0].data)
+        .data(sumstat[0].values)
     u
         .enter()
         .append("rect")
         .on('click', function (d) {
             console.log(d)
-            window.location.href = "second.html";
+            window.location.href = "third.html";
         })
         .merge(u)
         .transition()
         .duration(1000)
-            .attr("x", function(d) { return x(d["TransType"]); })
-            .attr("y", function(d) { return y(d["Count"]); })
-            .attr("width", x.bandwidth())
-            .attr("height", function(d) { return height - y(d["Count"]); })
+            .attr("x", function(d) { return x(d.key); })
+            .attr("y", function(d) { return y(d.value.count); })
+            .attr("width", 5)
+            // .attr("width", x.bandwidth())
+            .attr("height", function(d) { return height - y(d.value.count); })
             .attr("fill", "#69b3a2")
 
 }
